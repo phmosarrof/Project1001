1679555b2ad864605e718a20150793e097e2296e
Mosarrof Hossain <phmosharaf@gmail.com>
2018-08-10 20:04:35 -0400

    Encapsulation code added

--- src/encapsulation/RunEncap.java
+++ src/encapsulation/RunEncap.java
@@ -3,10 +3,10 @@ public class RunEncap {
 
    public static void main(String args[]) throws Exception {
 	  Encapsulation001 encap = new Encapsulation001();
-      encap.setName("James");
+      encap.setName("Mosarrof Hossain");
       encap.setAge(20);
-      encap.setIdNum("12343ms");
+      encap.setIdNum("12343090");
 
-      System.out.print("Name : " + encap.getName() + " Age : " + encap.getAge());
+      System.out.print(" Name : " + encap.getName() + " \n Age : " + encap.getAge());
    }
 }
